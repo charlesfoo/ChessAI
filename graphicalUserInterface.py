d610f10c63ff82cc0b3d12dc01a05f3a339f7964
Foo Zhi Yuan <fzy1995@gmail.com>
2018-01-26 04:18:42 -0600

    fixed grammatical error

--- graphicalUserInterface.py
+++ graphicalUserInterface.py
@@ -2,7 +2,7 @@
 
 """
 @author: Foo Zhi Yuan
-ChessAI implemented using the concepts of bitboard and principal variation search
+ChessAI is being implemented using the concepts of bitboard and principal variation search
 Requires Python 3 and Pillow(for GUI)
 USAGE: python chessAI.py to play in GUI and python chessAI_console.py to play in console
 """
